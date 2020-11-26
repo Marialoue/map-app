888731889a2be8a51240da50cbbaa073ff129042
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-11-26 08:44:02 +0100

    added userLocation

--- src/components/map.js
+++ src/components/map.js
@@ -1,25 +1,66 @@
-import React, {useState} from 'react';
-import ReactMapGL from 'react-map-gl';
-
-
-const mapToken = process.env.REACT_APP_TOKEN;
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import './map.css';
 
 export default function Map() {
-    // define a initial viewpoint
+
+    const mapToken = process.env.REACT_APP_TOKEN;
+
+    // defines an initial viewpoint
+    // lat and long will change when user is changing map view
     const [viewport, setViewport] = useState({
         width: 1000,
-        height: 400,
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 10
+        height: 450,
+        latitude: 59.330,
+        longitude: 18.075,
+        zoom: 12
     });
 
-    return(
-        <div className="map">
-            <ReactMapGL
-            {...viewport} 
-            onViewportChange={nextViewport => setViewport(nextViewport)}
-            mapboxApiAccessToken={mapToken}/>
+    // adding state for user location, where marker will show location
+    const [userLocation, setUserLocation] = useState({
+        userLat: null,
+        userLong: null
+    })
+
+    const handleClick = () => {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            setViewport({
+                ...viewport,
+                width: 1000,
+                height: 450,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                zoom: 15
+            })
+            setUserLocation({
+                ...userLocation,
+                userLat: position.coords.latitude,
+                userLong: position.coords.longitude
+            })
+        })
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Click to see your location</button>
+            <div className="map">
+                <ReactMapGL
+                    {...viewport}
+                    onViewportChange={nextViewport => setViewport(nextViewport)}
+                    mapboxApiAccessToken={mapToken}
+                >
+
+                    {/* add ternary to display marker only when userLocation is set */}
+                    <Marker
+                        latitude={viewport.latitude}
+                        longitude={viewport.longitude}
+                    >
+                        <PersonPinIcon />
+                    </Marker>
+                </ReactMapGL>
+            </div>
         </div>
     );
 }
