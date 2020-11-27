7267981ad2ba3961abc8714c942eb62f5c7c5fe8
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-11-27 14:16:32 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,37 @@
-// header with search bar
 import './Header.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Algolia from 'algolia-places-react';
 
-
-// autocomplete function
 const algoliaId = process.env.REACT_APP_ALGOLIA_ID;
 const algoliaApi = process.env.REACT_APP_ALGOLIA_API;
 
+/* todo:
+    * add state to save address in 
+    * on search - save address to state
+    * show address on map
+*/
 
 function Header() {
+
+    const [address, setAddress] = useState('');
 
     return (
         <div className="header">
             <h2>route finder</h2>
-            <Algolia
-                options={{
-                    appId: algoliaId,
-                    apiKey: algoliaApi,
-                    type: ['city', 'address']
-                }}
-            />
+            <div>
+                <Algolia
+                    placeholder=''
+                    options={{
+                        appId: algoliaId,
+                        apiKey: algoliaApi,
+                        type: ['city', 'address'],
+                        style: true // change to false for customized style - not implemented
+                    }}
+                    onChange={({ suggestion }) => {
+                        setAddress(suggestion.value);
+                    }}
+                />
+            </div>
         </div>
     );
 }
