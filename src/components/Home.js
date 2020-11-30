8082240879a0b75605d8db93b29d0c87154f9a89
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-11-30 11:44:36 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Map from './Map';
 import './Home.css';
 
 export default function Home() {
 
+    //  send address to map and setaddrees to header
+    const [address, setAddress] = useState({
+        addressLat: 51.5085,
+        addressLong: -0.12574
+    });
+
     return (
         <div className="home">
-            <Header />
-            <Map />
+                <Header setAddress={setAddress}/>
+                <Map address={address}/>
         </div>
     );
 }
