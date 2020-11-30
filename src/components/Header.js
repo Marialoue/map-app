8a3f7620477351ab8226256b362261bb12e279dc
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-11-30 11:44:24 +0100

    Update setAddress to suggested value

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,40 @@ import Algolia from 'algolia-places-react';
 const algoliaId = process.env.REACT_APP_ALGOLIA_ID;
 const algoliaApi = process.env.REACT_APP_ALGOLIA_API;
 
-/* todo:
-    * add state to save address in 
-    * on search - save address to state
-    * show address on map
-*/
 
-function Header() {
-
-    const [address, setAddress] = useState('');
+function Header({setAddress}) {
 
     return (
         <div className="header">
             <h2>route finder</h2>
             <div>
                 <Algolia
-                    placeholder=''
+                    placeholder='Where are you going?'
                     options={{
                         appId: algoliaId,
                         apiKey: algoliaApi,
                         type: ['city', 'address'],
-                        style: true // change to false for customized style - not implemented
+                        style: true, // change to false for customized style - not implemented
+                        aroundLatLngViaIP: true, // view suggestions closest to user via IP, true as default
                     }}
-                    onChange={({ suggestion }) => {
-                        setAddress(suggestion.value);
+
+                    onChange={({ query, rawAnswer, suggestion, suggestionIndex }) => {
+                        setAddress({
+                            addressLat: suggestion.latlng.lat,
+                            addressLong: suggestion.latlng.lng
+                        });
+                        console.log(`query: ${query}\n sugg name: ${suggestion.name}\n sugg value: ${suggestion.value}\n lat: ${suggestion.latlng.lat}\n long: ${suggestion.latlng.lng}\n`);
+                    }}
+
+                    onSuggestions={() => {
+                        console.log('onSuggestions fired')
+                    }}
+
+                    onLimit={(message) => {
+                        console.log('The rate limit has been reached: ', message)
+                    }}
+                    onError={(message) => {
+                        console.log('There was a problem retriving this request: ', message)
                     }}
                 />
             </div>
