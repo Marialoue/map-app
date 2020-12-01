7471d2f40122851270bd5948ec8a64a2be342fb2
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-12-01 10:18:04 +0100

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,20 +5,47 @@ import './Map.css';
 
 export default function Map({ address }) {
 
+    // for mapbox
     const mapToken = process.env.REACT_APP_TOKEN;
 
-    console.log('this is map.js\n address: ', address);
+    // for here api
+    const hereKey = process.env.REACT_APP_HERE_KEY;
+    const hereId = process.env.REACT_APP_HERE_APPID;
 
     /* TODO
-        show searched location on map with coords from address
+        add routing to map
+        lat and lng coords from current position to destination / searched address (origin, departure, transport mode)
+        also add polygon layer to map to demonstrate route
+    */
+
+    // options to be used in hereUrl - origin should maybe be {userLocation}, destination should be {address}
+    const options = {
+        transport: 'car',
+        origin: {
+            lat: '',
+            lng: ''
+        },
+        destination: {
+            lat: '',
+            lng: ''
+        },
+    };
+
+    const hereUrl = `https://router.hereapi.com/v8/routes?transportMode=${options.transport}&origin=${options.origin.lat},${options.origin.lng}&destination=${options.destination.lat},${options.destination.lng}&apiKey=${hereKey}&return=summary`
+
+    /*
+        &return=summary (optinally attribute showing duration, lenght, and baseDuration - returns summary of entire travel section incl pre- and post actions)
+        &return=travelSummary (returns summary only of travel portion i.e no pre- or post action)
+        &return=polyline (https://github.com/heremaps/flexible-polyline)
+        &apiKey=REACT_APP_HERE_KEY
     */
 
     // defines an initial viewpoint - lat and long will change when user is changing map view
     const [viewport, setViewport] = useState({
         width: 1000,
         height: 450,
-        latitude: address.addressLat,
-        longitude: address.addressLong,
+        latitude: 51.5085,
+        longitude: -0.12574,
         zoom: 12
     });
 
@@ -61,6 +88,17 @@ export default function Map({ address }) {
                             <PersonPinIcon />
                         </Marker>) : (null)
                     }
+
+                    {address.addressLat ? (
+                        <Marker
+                            latitude={address.addressLat}
+                            longitude={address.addressLong}
+                        >
+                            <PersonPinIcon />
+                        </Marker>
+                    ) : (null)
+                    }
+
                 </ReactMapGL>
             </div>
         </div>
