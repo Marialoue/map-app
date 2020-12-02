25c3c5ba52c524cefd72789ef021809c97ee5c19
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-12-02 10:50:26 +0100

    added imported buttons

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,37 +1,44 @@
 import React, { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
+import PersonPinCircleRoundedIcon from '@material-ui/icons/PersonPinCircleRounded';
+import RoomRoundedIcon from '@material-ui/icons/RoomRounded';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import './Map.css';
 
 export default function Map({ address }) {
+
+    // adding state for user location, where marker will show location
+    const [userLocation, setUserLocation] = useState({
+        latitude: null,
+        longitude: null
+    })
 
     // for mapbox
     const mapToken = process.env.REACT_APP_TOKEN;
 
     // for here api
     const hereKey = process.env.REACT_APP_HERE_KEY;
-    const hereId = process.env.REACT_APP_HERE_APPID;
 
     /* TODO
         add routing to map
-        lat and lng coords from current position to destination / searched address (origin, departure, transport mode)
         also add polygon layer to map to demonstrate route
     */
 
-    // options to be used in hereUrl - origin should maybe be {userLocation}, destination should be {address}
+    // options to be used in hereUrl
     const options = {
         transport: 'car',
         origin: {
-            lat: '',
-            lng: ''
+            lat: userLocation.userLat,
+            lng: userLocation.userLong
         },
         destination: {
-            lat: '',
-            lng: ''
+            lat: address.addressLat,
+            lng: address.addressLong
         },
     };
 
-    const hereUrl = `https://router.hereapi.com/v8/routes?transportMode=${options.transport}&origin=${options.origin.lat},${options.origin.lng}&destination=${options.destination.lat},${options.destination.lng}&apiKey=${hereKey}&return=summary`
+    const hereUrl = `https://router.hereapi.com/v8/routes?transportMode=${options.transport}&origin=${options.origin.lat},${options.origin.lng}&destination=${options.destination.lat},${options.destination.lng}&apiKey=${hereKey}&return=summary,polyline,actions,instructions`
 
     /*
         &return=summary (optinally attribute showing duration, lenght, and baseDuration - returns summary of entire travel section incl pre- and post actions)
@@ -42,18 +49,12 @@ export default function Map({ address }) {
 
     // defines an initial viewpoint - lat and long will change when user is changing map view
     const [viewport, setViewport] = useState({
-        width: 1000,
-        height: 450,
-        latitude: 51.5085,
-        longitude: -0.12574,
-        zoom: 12
+        height: '84.5vh',
+        width: '100vw',
+        latitude: 18.0767,
+        longitude: -10.9782,
+        zoom: 1.3
     });
-
-    // adding state for user location, where marker will show location
-    const [userLocation, setUserLocation] = useState({
-        latitude: null,
-        longitude: null
-    })
 
     const handleClick = () => {
 
@@ -71,21 +72,35 @@ export default function Map({ address }) {
         })
     };
 
+    const handleSearch = () => {
+        address &&
+            address.addressLat ? (fetch(hereUrl)
+                .then(response => response.json())
+            ) : (
+                alert('oh no, you forgot to enter a destination')
+            );
+    };
+
     return (
         <div>
-            <button onClick={handleClick}>Click to see your location</button>
+            <ButtonGroup variant="text" color="default" size="small" aria-label="text primary button group">
+                <Button onClick={handleClick}>Find my location</Button>
+                <Button onClick={handleSearch}>Find route</Button>
+            </ButtonGroup>
+
             <div className="map">
                 <ReactMapGL
                     {...viewport}
                     onViewportChange={nextViewport => setViewport(nextViewport)}
                     mapboxApiAccessToken={mapToken}
+                    // mapStyle='mapbox://styles/mapbox/light-v9'
                 >
                     {userLocation.userLat ? (
                         <Marker
                             latitude={userLocation.userLat}
                             longitude={userLocation.userLong}
                         >
-                            <PersonPinIcon />
+                            <PersonPinCircleRoundedIcon />
                         </Marker>) : (null)
                     }
 
@@ -94,7 +109,7 @@ export default function Map({ address }) {
                             latitude={address.addressLat}
                             longitude={address.addressLong}
                         >
-                            <PersonPinIcon />
+                            <RoomRoundedIcon />
                         </Marker>
                     ) : (null)
                     }
