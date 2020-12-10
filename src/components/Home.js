ae78a577ca71301e5edc55ef8fd8a12ef55c5270
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-12-10 16:25:08 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import Map from './Map';
-import './Home.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import Map from "./Map";
+import "./Home.css";
 
 export default function Home() {
+  //  address to be used in Map, and setAddrees in Header
+  const [address, setAddress] = useState({
+    addressLat: null,
+    addressLong: null,
+  });
 
-    //  address to be used in Map, and setaddrees in Header
-    const [address, setAddress] = useState({
-        addressLat: null,
-        addressLong: null
-    });
+  // polylineCoords to be used in Map for geojson, setPolylineCoords in Header when
+  const [polylineCoords, setPolylineCoords] = useState([0, 0]);
 
-    // add a theme to set light or dark theme 
+  // add a theme to set light or dark theme
 
-    return (
-        <div className="home">
-            <Header
-                setAddress={setAddress}
-            />
-            <Map
-                address={address}
-            />
-        </div>
-    );
+  return (
+    <div className="home">
+      <Header setAddress={setAddress} setPolylineCoords={setPolylineCoords} />
+      <Map
+        address={address}
+        setPolylineCoords={setPolylineCoords}
+        polylineCoords={polylineCoords}
+      />
+    </div>
+  );
 }
