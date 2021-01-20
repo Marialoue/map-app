1a75db938feb949c57c5022608213aad3384d300
marialoue <mariahkarlsson_@hotmail.com>
2021-01-20 13:10:00 +0100

    Remove App.css import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
 import Home from './components/Home';
 
 export default function App() {
     return (
-        <div className="App">
             <Home />
-        </div>
     );
 }
