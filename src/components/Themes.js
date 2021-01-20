8ddb83ce0ca0722a8afd1ff14efbd90d58504aa9
marialoue <mariahkarlsson_@hotmail.com>
2021-01-20 20:02:11 +0100

    Create Themes.js
    Adding variables for the different themes

---
+++ src/components/Themes.js
@@ -0,0 +1,23 @@
+// variables for light and dark themes
+
+export const lightTheme = {
+  mapStyle: "mapbox://styles/mapbox/light-v8",
+  lineColor: "rgba(3, 170, 238, 0.5)",
+  color: "#121212",
+  background: "linear-gradient(#787878, 40%, transparent)",
+  btnValue: "Light",
+  fill: "#121212",
+  svgColor: "#121212",
+  navColor: "rgba(183, 230, 249, 0.5)",
+};
+
+export const darkTheme = {
+  mapStyle: "mapbox://styles/mapbox/dark-v9",
+  lineColor: "rgba(255, 125, 69, 0.6)",
+  color: "#bebebe",
+  background: "linear-gradient(black, 40%, transparent)",
+  btnValue: "Dark",
+  fill: "#959da5",
+  svgColor: "#959da5",
+  navColor: "rgba(251, 213, 196, 0.5)"
+};
