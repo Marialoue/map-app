1ec192ab762139184580c9ef67cf2d95e9e8f295
Maria Karlsson <40427644+Marialoue@users.noreply.github.com>
2022-02-28 12:25:01 +0100

    Remove Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import Home from './components/Home';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Map from "./components/Map";
+import Theme from "./styles/GlobalStyles";
 
-export default function App() {
-    return (
-            <Home />
-    );
+import { LocationProvider } from "./context/LocationContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
+export default function Home() {
+  // polylineCoords to be used in Map for geojson, setPolylineCoords is used in Header when user has searched a destination
+  const [polylineCoords, setPolylineCoords] = useState([0, 0]);
+
+  return (
+    <ThemeProvider>
+      <Theme>
+        <Header />
+        <LocationProvider>
+          <Map
+            polylineCoords={polylineCoords}
+            setPolylineCoords={setPolylineCoords}
+          />
+        </LocationProvider>
+      </Theme>
+    </ThemeProvider>
+  );
 }
