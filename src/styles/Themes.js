296fca43aa73f73506db4f81ce0a44eb87f5238f
Maria Karlsson <40427644+Marialoue@users.noreply.github.com>
2022-02-28 12:25:54 +0100

    Move to style folder

---
+++ src/styles/Themes.js
@@ -0,0 +1,20 @@
+export const themes = {
+  light: {
+    mapStyle: "mapbox://styles/mapbox/light-v10",
+    lineColor: "rgba(3, 170, 238, 0.5)",
+    color: "#121212",
+    background: "linear-gradient(180deg, rgba(91, 88, 88, 0.745), 40%, transparent)",
+    fill: "#121212",
+    svgColor: "#121212",
+    navColor: "rgba(183, 230, 249, 0.5)",
+  },
+  dark: {
+    mapStyle: "mapbox://styles/mapbox/dark-v10",
+    lineColor: "rgba(255, 125, 69, 0.6)",
+    color: "#bebebe",
+    background: "linear-gradient(180deg, black, 50%, transparent)",
+    fill: "#959da5",
+    svgColor: "#959da5",
+    navColor: "rgba(251, 213, 196, 0.5)",
+  }
+};
