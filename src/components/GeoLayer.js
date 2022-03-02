138db520039513dc606897a39b6fb57c99b46a52
Maria Karlsson <40427644+Marialoue@users.noreply.github.com>
2022-03-02 13:36:17 +0100

    Use themeContext for geolayer

--- src/components/GeoLayer.js
+++ src/components/GeoLayer.js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import { Source, Layer } from "react-map-gl";
 
-const GeoLineLayer = ({ destination, polylineCoords, isDarkMode }) => {
+import { ThemeContext } from "../context/ThemeContext";
+
+const GeoLineLayer = ({ destination, polylineCoords }) => {
+  const { theme, lineColor } = useContext(ThemeContext);
+
   const geojsonLine = {
     type: "FeatureCollection",
     features: [
@@ -13,41 +18,26 @@ const GeoLineLayer = ({ destination, polylineCoords, isDarkMode }) => {
       },
     ],
   };
+
+  const layerStyle = {
+    id: "line",
+    type: "line",
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+    },
+    paint: {
+      "line-color": `${lineColor[theme]}`,
+      "line-width": 5,
+    },
+  };
+
   return (
     <>
-      {/* showing polylineLayer when destination is validated and btn (handleSearch) is clicked */}
       {destination.lat ? (
-        isDarkMode ? (
-          <Source id="polylineLayer" type="geojson" data={geojsonLine}>
-            <Layer
-              id="line"
-              type="line"
-              layout={{
-                "line-join": "round",
-                "line-cap": "round",
-              }}
-              paint={{
-                "line-color": "rgba(255, 125, 69, 0.6)",
-                "line-width": 5,
-              }}
-            />
-          </Source>
-        ) : (
-          <Source id="polylineLayer" type="geojson" data={geojsonLine}>
-            <Layer
-              id="line"
-              type="line"
-              layout={{
-                "line-join": "round",
-                "line-cap": "round",
-              }}
-              paint={{
-                "line-color": "rgba(3, 170, 238, 0.5)",
-                "line-width": 5,
-              }}
-            />
-          </Source>
-        )
+        <Source id="polylineLayer" type="geojson" data={geojsonLine}>
+          <Layer {...layerStyle} />
+        </Source>
       ) : null}
     </>
   );
